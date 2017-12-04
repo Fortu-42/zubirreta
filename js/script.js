3cf60af15f8e41fbdd22189582e0b094e1b9dc88
Fortu-42 <fortunatoherrerap@gmail.com>
2017-12-04 18:14:54 -0400

    zready

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        nav:true,
+        nav:false,
         center:true,
         items: 1,
-        lazyLoad:false
+        lazyLoad:false,
+        autoplay:true,
+        autoplayTimeout:7000
     });
 });
